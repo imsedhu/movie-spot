67b0b419f58d39580a97b7b58d9ba21d9e67d4c6
imsedhu <sedhuramansedhu17@gmail.com>
2024-07-26 20:53:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1 className="text-3xl font-bold underline">movie spot</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
