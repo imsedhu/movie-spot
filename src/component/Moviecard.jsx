9240dfed87d4be008fa50149bc63b342c9cc8cd4
imsedhu <sedhuramansedhu17@gmail.com>
2024-08-07 20:59:08 +0530

    popular movie cards added

--- src/component/Moviecard.jsx
+++ src/component/Moviecard.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 
-const Moviecard = () => {
+const Moviecard = ({poster_path, name}) => {
   return (
-    <div className='w-[180px] h-[60vh] bg-cover bg-center rounded-xl hover:scale-110 transition duration-300'  style={{backgroundImage: `url(https://i.ebayimg.com/images/g/CU8AAOSwPLtc5CfI/s-l400.jpg)`}}>
+    <div className='w-[180px] h-[60vh] bg-cover bg-center rounded-xl hover:scale-110 transition duration-300 flex items-end '  style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})`}}>
+
+      <div className='w-full text-center text-white text-xl bg-gray-900/60 '>
+        {name}
+      </div>
 
     </div>
   )
--- src/component/Movies.jsx
+++ src/component/Movies.jsx
@@ -1,26 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import Moviecard from './Moviecard'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Movies = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=3e935957054458838aa7447a954fb270&language=en-US&page=6`).then(function(res){
+      console.log(res.data.results);
+      setMovies(res.data.results)
+    }, [])
+  })
   return (
     <>
-    <p className='text-center font-bold text-2xl my-8'>Trending Movies</p>
+    <p className='text-center font-bold text-2xl my-8'> Trending Movies</p>
     <div className=' pl-8 flex flex-wrap gap-8'>
+
+      {movies.map((movie, index)=>{
+        return <Moviecard key={index}  poster_path={movie.poster_path} name={movie.original_title}/>
+      })}
       
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
-      <Moviecard />
     </div>
 
     </>
@@ -28,3 +30,5 @@ const Movies = () => {
 }
 
 export default Movies
+
+//https://api.themoviedb.org/3/movie/popular?api_key=3e935957054458838aa7447a954fb270&language=en-US&page=1
